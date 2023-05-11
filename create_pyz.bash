81d01f31158522933fbe0770f2290c197779a510
Mathias Jeschke <mjeschke@juniper.net>
2023-05-11 15:11:57 +0200

    Adjusted pyz creation script

--- create_pyz.bash
+++ create_pyz.bash
@@ -1,8 +1,36 @@
 #!/bin/bash
 
 tmpdir=$(mktemp -d) || exit 1
-script=$(ls -1 *.py | sed -n '1s/\.py//p')
-cp $script.py $tmpdir/__main__.py
-python3 -m pip install -r requirements.txt --target $tmpdir
-python3 -m zipapp --compress --python "/usr/bin/env python3" --output $script.pyz $tmpdir
+
+# check if python supports zipapp compression
+zipapp="python3 -m zipapp"
+if python3 -m zipapp --help | grep -q -- --compress; then
+    zipapp="$zipapp --compress"
+fi
+
+if [ -d lib ]; then cp -a lib $tmpdir/; fi
+
+if [ -z $requirements ]; then
+  if [ -f requirements.txt ]; then
+    requirements=$(cat requirements.txt)
+    if python3 -c 'import requests' 2>/dev/null; then
+        requirements=$(grep -iv requests requirements.txt)
+    fi
+  fi
+fi
+if [ -n "$requirements" ]; then
+  python3 -m pip install $requirements --target $tmpdir
+fi
+
+if [ $# -lt 1 ]; then
+  scripts=*.py
+else
+  scripts="$@"
+fi
+
+for script in $scripts; do
+  cp $script $tmpdir/__main__.py
+  $zipapp --python "/usr/bin/env python3" --output ${script}z $tmpdir
+done
+
 rm -r $tmpdir
